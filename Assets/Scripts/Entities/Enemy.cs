883012b912bc3dc8c3d1a6338d7dd34a5e367a07
hernan3396 <hernan3396@gmail.com>
2022-05-01 11:53:26 -0300

    creado enemigo, lee stats y apunta al target

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Parameters
+    [Header("Parameters")]
+    [SerializeField] private EnemyData _enemyData;
+    // parece que estan medios desordenados pero es el
+    // orden que estan en el scriptable object
+    // asi mantenemos coherencia entre ambos
+    private int _hp;
+    private int _damage;
+    private float _fireRate;
+    private int _movSpeed;
+    private float _accuracy;
+    #endregion
+
+    #region BodyParts
+    [Header("Body Parts")]
+    [SerializeField] private Transform _arm;
+    private Transform _transform;
+    #endregion
+
+    private Transform _targetPos;
+
+    private void Awake()
     {
-        
+        SetStats();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _targetPos = GameManager.GetInstance.GetPlayerPos;
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Aim();
+    }
+
+    private void SetStats()
+    {
+        _hp = _enemyData.HP;
+        _damage = _enemyData.DAMAGE;
+        _fireRate = _enemyData.FIRERATE;
+        _movSpeed = _enemyData.MOVSPEED;
+        _accuracy = _enemyData.ACCURACY;
+    }
+
+    private void Aim()
+    {
+        Vector3 dir = _targetPos.position - _transform.position;
+
+        DOTween.To(() => _arm.right, x => _arm.right = x, dir, _accuracy);
+        // _arm.right = test;
     }
 }
