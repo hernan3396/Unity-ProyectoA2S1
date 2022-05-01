eaa439f6d52640b761e53bdda2230db92a49f7ba
hernan3396 <hernan3396@gmail.com>
2022-05-01 11:53:40 -0300

    movi unas carpetas de lugar

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _instance;
+
+    #region Components
+    [SerializeField] private Transform _playerPos;
+    #endregion
+
+    private void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (_instance != this)
+        {
+            _instance = null;
+        }
+    }
+
+    public static GameManager GetInstance
+    {
+        get { return _instance; }
+    }
+
+    public Transform GetPlayerPos
+    {
+        get { return _playerPos; }
     }
 }
