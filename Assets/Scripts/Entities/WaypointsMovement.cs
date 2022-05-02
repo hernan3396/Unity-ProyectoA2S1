99d738cfd732dc948ba0717a8dd81ec3d7b762bc
hernan3396 <hernan3396@gmail.com>
2022-05-02 09:31:18 -0300

    ruta del enemigo usando NavMesh

---
+++ Assets/Scripts/Entities/WaypointsMovement.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+public class WaypointsMovement : MonoBehaviour
+{
+    #region Components
+    private Enemy _enemy; // esta para recibir la info si el enemigo esta a la vista
+    // realmente habria que mover las cosas a scripts mas ordenados
+    #endregion
+
+    #region Waypoints
+    [Header("Waypoints")]
+    [SerializeField] private Transform _waypointsContainer;
+    private List<Transform> _waypoints = new List<Transform>();
+    #endregion
+
+    #region Parameters
+    [Header("Parameters")]
+    [SerializeField] private float _newWaypointSpeed = 1;
+    #endregion
+
+    private int _currentWaypoint = 0;
+    private bool _isMoving = false;
+    private NavMeshAgent _agent;
+
+    private void Awake()
+    {
+        _enemy = GetComponent<Enemy>();
+        _agent = GetComponent<NavMeshAgent>();
+
+        foreach (Transform waypoint in _waypointsContainer)
+        {
+            if (waypoint.TryGetComponent(out MeshRenderer meshRenderer))
+                meshRenderer.enabled = false;
+
+            _waypoints.Add(waypoint);
+        }
+
+        StartMovement();
+    }
+
+    private void Update()
+    {
+        // si ve al enemigo frena, sino sigue su ruta
+        // esto esta horrible aca puesto pero funciona
+        // cambiar luego
+        if (_enemy.EnemyOnSight)
+        {
+            _agent.isStopped = true;
+            _enemy.GetRB.velocity = Vector3.zero;
+            return;
+        }
+        _agent.isStopped = false;
+
+        if (!_isMoving) return;
+
+        if (_agent.remainingDistance <= 0.2f)
+        {
+            _isMoving = false;
+            _currentWaypoint += 1;
+
+            if (_currentWaypoint >= _waypoints.Count)
+                _currentWaypoint = 0;
+
+            Invoke("StartMovement", 1);
+        }
+    }
+
+    private void StartMovement()
+    {
+        if (_isMoving) return;
+
+        _isMoving = true;
+        _agent.SetDestination(_waypoints[_currentWaypoint].position);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(_waypointsContainer.GetChild(0).position, 0.6f);
+
+        Gizmos.color = Color.red;
+
+        Vector3 previousWaypoint = _waypointsContainer.GetChild(0).position;
+        int index = 0;
+
+        foreach (Transform waypoint in _waypointsContainer)
+        {
+            if (index != 0)
+            {
+                Gizmos.DrawLine(previousWaypoint, waypoint.position);
+                previousWaypoint = waypoint.position;
+            }
+
+            index++;
+        }
+
+        Gizmos.DrawLine(previousWaypoint, _waypointsContainer.GetChild(0).position);
+    }
+}
