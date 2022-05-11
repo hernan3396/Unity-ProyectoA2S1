a252251acf0e846d7dd2963f2f7003b3cd0cc90e
hernan3396 <hernan3396@gmail.com>
2022-05-11 20:25:52 -0300

    arreglado?

--- Assets/Scripts/Entities/MeleeDamage.cs
+++ Assets/Scripts/Entities/MeleeDamage.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class MeleeDamage : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     if (other.collider.CompareTag("Enemy"))
+    //         other.collider.GetComponent<Enemy>().TakeDamage(10);
+    // }
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.collider.CompareTag("Enemy"))
-            other.collider.GetComponent<Enemy>().TakeDamage(10);
+        if (other.CompareTag("Enemy"))
+            other.GetComponent<Enemy>().TakeDamage(10);
     }
 }
