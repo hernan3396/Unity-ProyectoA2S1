6e834e0821dfba25b01cd35fd6a63b7cd9f0a046
hernan3396 <hernan3396@gmail.com>
2022-05-21 20:35:17 -0300

    cambiado player al nuevo "sistema"

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using DG.Tweening;
 using System.Collections;
 
 public class Player : Entity
 {
     #region Components
+    private InventoryManager _invManager;
     private UIController _uiController;
     private Rigidbody _rb;
     private Inputs _input;
@@ -18,20 +20,69 @@ public class Player : Entity
     #endregion
 
     #region BodyParts
-    private Transform _particlePossOff;
+    [SerializeField] private Transform _particlePosOff;
+    [SerializeField] private Transform _shootingPos;
+    [SerializeField] private Transform _meleeArm;
+    [SerializeField] private Transform _arm;
+    #endregion
+
+    #region Jumping
+    private bool _isGrounded = true;
+    private bool _jumping = false;
+    private PoolManager _dustPool;
+    private float _jumpTimer;
     #endregion
 
     #region RocketJumping
-    private float _rocketJumpingTimer;
+    private bool _isRocketJumping;
     #endregion
 
+    #region Aiming
+    private bool _canShoot = true;
+    private Vector3 _aimPosition;
+    private bool _isMouse = true; // para ver que tipo de input estas usando
+    #endregion
+
+    #region Melee
+    [Header("Melee")]
+    // esta parte se va a ver cambiada cuando tengamos la animacion
+    // lo mas seguro es que lo hagamos desde ahi
+    [SerializeField] private Vector3 _meleeFinalPos; // el angulo final
+    [SerializeField] private float _meleeSpeed = 1; // el tiempo que demora en "Hacer" la animacion del melee
+    private Quaternion _meleeInitialRot; // el angulo del que empieza
+    private bool _canMelee = true;
+    #endregion
+
+    #region Crouching
+    [Header("Crouching")]
+    [SerializeField] private GameObject _crouchingHitbox;
+    [SerializeField] private GameObject _standingHitbox;
+    private bool _crouching = false; // se podria hacer sin esta variable pero asi se ejecuta solo las veces necesarias el metodo Crouch()
+    #endregion
 
     protected override void Awake()
     {
         base.Awake();
         SetStats();
+
+        _rb = GetComponent<Rigidbody>();
     }
 
+    protected override void Start()
+    {
+        base.Start();
+        _uiController = GameManager.GetInstance.GetUIController;
+        _invManager = GameManager.GetInstance.GetInvManager;
+        _dustPool = GameManager.GetInstance.GetDustPool;
+        _cam = GameManager.GetInstance.GetMainCamera;
+        _input = GameManager.GetInstance.GetInput;
+
+        _uiController.UpdateHealthPoints(_currentHP);
+
+        _input.OnControlChanged += ControlChanged;
+    }
+
+    #region Parameters
     private void SetStats()
     {
         _hp = _playerData.Hp;
@@ -44,48 +95,234 @@ public class Player : Entity
 
         _speed = _playerData.Speed;
 
-        _rocketJumpingTimer = _playerData.RocketJumpingTimer;
-
         _weaponList = _playerData.WeaponList;
     }
+    #endregion
 
-    protected override void Move()
+    private void Update()
     {
-        throw new System.NotImplementedException();
+        Aim();
+
+        if (_isGrounded && _input.jump)
+            Jump();
+
+        if (_isGrounded && _input.Crouching)
+            Crouch(true);
+
+        if (_crouching && !_input.Crouching)
+            Crouch(false);
+
+        if (_jumping)
+        {
+            _jumpTimer -= Time.deltaTime;
+
+            if (_jumpTimer <= 0 || !_input.jump)
+                StopJump();
+        }
+
+        if (_canShoot)
+        {
+            if (_input.IsShooting)
+            {
+                StartCoroutine(Shoot(_weaponList[(int)WeaponData.Weapons.TwinPistols]));
+                return;
+            }
+
+            if (_input.CannonShooting)
+            {
+                StartCoroutine(Shoot(_weaponList[(int)WeaponData.Weapons.RocketLauncher]));
+                return;
+            }
+        }
+
+        if (_canMelee && _input.Melee) { };
+        // melee
     }
 
+    private void FixedUpdate()
+    {
+        Move();
+
+        _rb.AddForce(Physics.gravity * _gravityScale, ForceMode.Acceleration); // simula una gravedad mas pesada
+    }
+
+    #region HorizontalMovement
+    protected override void Move()
+    {
+        if (_isRocketJumping) return;
+        _rb.velocity = new Vector3(_input.move.x * _speed, _rb.velocity.y);
+    }
+    #endregion
+
+    #region Grounded
+    // para no andar chambiando con _isGrounded = !_isGrounded
+    // porque asumo que puede llegar a dar algun problema
+    public void IsGrounded()
+    {
+        _isGrounded = true;
+        _isRocketJumping = false;
+    }
+
+    public void NotGrounded()
+    {
+        _isGrounded = false;
+    }
+    #endregion
+
+    #region Jumping
+    private void Jump()
+    {
+        _jumping = true;
+        _jumpTimer = _jumpTime; // timer para limitar el salto
+        _rb.velocity = new Vector3(_rb.velocity.x, _jumpForce, _rb.velocity.z);
+
+        GameObject dust = _dustPool.GetPooledObject();
+        if (!dust) return;
+
+        // sino aparece en el centro
+        Vector3 dustPosition = _particlePosOff.position;
+        dust.transform.position = dustPosition;
+        dust.SetActive(true);
+    }
+
+    private void StopJump()
+    {
+        _jumping = false;
+        _jumpTimer = 0;
+        // _rb.velocity = new Vector3(_rb.velocity.x, _jumpForce * 0.5f, _rb.velocity.z); // pegaba un minisalto cuando lo soltabas 
+        _rb.velocity = new Vector3(_rb.velocity.x, _rb.velocity.y * 0.5f, _rb.velocity.z);
+    }
+    #endregion
+
+    #region Aiming
     protected override void Aim()
     {
-        throw new System.NotImplementedException();
+        if (_isMouse)
+        {
+            Ray ray = _cam.ScreenPointToRay(_input.look);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+                _aimPosition = (Vector2)hit.point;
+
+            _aimPosition.z = 0;
+            _arm.right = _aimPosition - _arm.position;
+        }
+        else
+        {
+            // control
+            _arm.right = _input.look;
+        }
     }
 
     protected override IEnumerator Shoot(WeaponData weaponData)
     {
-        yield return null;
+        // chequea si hay balas
+        if (_invManager.GetAmount((int)weaponData.BulletType) <= 0) yield break;
+
+        // si hay balas sigue adelante
+        _canShoot = false;
+
+        Vector3 bulletDirection = _arm.right;
+        _shooting.Shoot((int)weaponData.BulletType, _shootingPos.position, bulletDirection, weaponData.BulletSpeed);
+
+        // consume una bala del inventario
+        _invManager.RemoveAmount((int)weaponData.BulletType, 1);
+
+        yield return new WaitForSeconds(weaponData.FireRate);
+
+        _canShoot = true;
+    }
+    #endregion
+
+    #region ControlChanged
+    private void ControlChanged(string value)
+    {
+        // DualShock4GamepadHID nombre del control de ps4
+        switch (value)
+        {
+            case "Keyboard":
+                _isMouse = true;
+                break;
+            default:
+                _isMouse = false;
+                break;
+        }
+    }
+    #endregion
+
+    #region Damage
+    public void AddHealth(int value)
+    {
+        if (_currentHP + value <= _hp)
+        {
+            _currentHP += value;
+        }
+        else
+        {
+            _currentHP = _hp;
+        }
+
+        _uiController.UpdateHealthPoints(_currentHP);
     }
 
-    protected override IEnumerator Melee()
+    public override void TakeDamage(int value)
     {
-        throw new System.NotImplementedException();
+        base.TakeDamage(value);
+        _uiController.UpdateHealthPoints(_currentHP);
     }
 
     protected override void Death()
     {
-        throw new System.NotImplementedException();
+        GameManager.GetInstance.GameOver();
     }
+    #endregion
 
+    #region Melee
+    protected override IEnumerator Melee()
+    {
+        // falta que use la direccion del mouse
+        // o la rotacion del personaje
+        _canMelee = false;
+
+        // rota el "Bate"
+        _meleeArm.gameObject.SetActive(true);
+        _meleeArm.DORotate(_meleeFinalPos, _meleeSpeed);
+
+        yield return new WaitForSeconds(_meleeSpeed);
+
+        // lo devuelve a su posicion inicial
+        _meleeArm.gameObject.SetActive(false);
+        _meleeArm.rotation = _meleeInitialRot;
+
+        _canMelee = true;
+    }
+    #endregion
+
+    #region Crouching
+    private void Crouch(bool value)
+    {
+        _crouchingHitbox.SetActive(value);
+        _standingHitbox.SetActive(!value);
+        _crouching = value;
+    }
+    #endregion
+
+    #region RocketJumping
     public void RocketJumping(bool value)
     {
-        Debug.Log("Test");
+        _isRocketJumping = value;
     }
+    #endregion
 
-    public void AddHealth(int value)
+    private void OnDestroy()
     {
-        Debug.Log("Test");
+        _input.OnControlChanged -= ControlChanged;
     }
 
+    #region Getter/Setter
     public Rigidbody GetRB
     {
         get { return _rb; }
     }
+    #endregion
 }
