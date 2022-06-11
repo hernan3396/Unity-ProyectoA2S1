20c472b43092969a526e504e1c334af585df2e73
hernan3396 <hernan3396@gmail.com>
2022-06-11 16:59:20 -0300

    cambios a las plataformas

---
+++ Assets/Scripts/Shooting/AmmoBarrel.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AmmoBarrel : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPosition;
+    [SerializeField] private Vector2 _randomDirection;
+    [SerializeField] private int _force;
+    private PoolManager _ammoPool;
+
+    private void Start()
+    {
+        _ammoPool = GameManager.GetInstance.GetAmmoPool;
+    }
+
+    public void SpawnAmmo()
+    {
+        GameObject ammo = _ammoPool.GetPooledObject();
+        ammo.transform.position = (Vector2)_spawnPosition.position;
+        ammo.SetActive(true);
+
+        Vector2 direction = new Vector2(Random.Range(_randomDirection.x, _randomDirection.y), _force);
+        ammo.GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
+    }
+}
